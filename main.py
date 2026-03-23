b232a016c715f347a91d74dcb9868d9b40961ce5
Sabreesh Raj Nagarajan <sabreeshrajnagarajan@gmail.com>
2026-03-23 14:14:51 +0530

    Revert "Committed on 22 March after final implementation"
    This reverts commit 5d1b68283e0c82542c36bf5918250fc1c44e2546.

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 import threading
 import time
 
-from vehicles.vehicle_B import start_udp_server
-from vehicles.vehicle_A import start_udp_client
+from vehicles.vehicle_B import start_tls_server
+from vehicles.vehicle_A import start_tls_client
 
 
 def run_simulation():
 
-    print("\n" + "=" * 60)
-    print("  Post-Quantum Secure V2V Platoon Communication")
-    print("  Protocol: ML-KEM-768 (Kyber) + AES-256-GCM over UDP")
-    print("=" * 60)
+    print("\n=== Starting Secure V2V Communication ===")
 
-    # Start Vehicle B (Follower/Server) in a background thread
-    server_thread = threading.Thread(target=start_udp_server, daemon=True)
+    server_thread = threading.Thread(target=start_tls_server)
     server_thread.start()
 
-    # Give the server time to bind
-    time.sleep(1)
+    time.sleep(2)
 
-    # Start Vehicle A (Leader/Client) in the main thread
-    start_udp_client()
+    start_tls_client()
 
 
 if __name__ == "__main__":
